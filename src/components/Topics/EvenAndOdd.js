703273a0366362bd85cb26763f8e1242e0c728f4
Simon <everett.blake@gmail.com>
2018-05-18 12:35:36 -0600

    evenAndOdd done

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -17,22 +17,32 @@ class EvenAndOdd extends Component {
 
 
     render() {
+
+        // const newEvenArray = this.state.evenArray.map((e, i) => 
+        //     (<li key={`evenArray ${i}`}>{e}, </li>))
+        // const newOddArray = this.state.oddArray.map((e, i) => 
+        // (<li key={`oddArray ${i}`}>{e}, </li>))
+
         return (
             <div className="puzzleBox evenAndOddPB">
                 <h4>
                     Evens And Odds
                 </h4>
-                <input className="inputLine" onChange={(e) => (handleChange(e))} > 
+                <input 
+                        className="inputLine" 
+                        onChange={e => this.handleChange(e)} 
+                        value={this.state.userInput}
+                        > 
 
                 </input>
-                <button className="confirmationButton">
-
+                <button className="confirmationButton" onClick={() => this.assignInput(this.state.userInput)}>
+                    split
                 </button>
                 <span className="resultBox">
-
+                    Evens: {this.state.evenArray.toString()}
                 </span>
                 <span className="resultBox">
-
+                    Odds: {this.state.oddArray.toString()}
                 </span>
             </div>
         )
@@ -41,6 +51,25 @@ class EvenAndOdd extends Component {
 
     handleChange(e) {
         this.setState({userInput: e.target.value});
+    }
+
+    assignInput(userInput) {
+        let splitString = userInput.split(',');
+        let evens = [];
+        let odds = [];
+
+        for(var i=0; i<splitString.length; i++){
+            if(splitString[i] % 2 ===0){
+                evens.push(parseInt(splitString[i], 10));
+            } else {
+                odds.push(parseInt(splitString[i], 10));
+            }
+        }
+        this.setState({
+            evenArray: evens,
+            oddArray: odds,
+            userInput: '',
+        })
     }
     
 }
